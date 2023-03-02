502579577b93e57d33c187d0cc29829a00c1961f
saiminh <saiminh@gmail.com>
2023-03-02 16:51:22 +0100

    back to src/static

--- src/_includes/_components/shortcodes/imageShortcode.js
+++ src/_includes/_components/shortcodes/imageShortcode.js
@@ -4,7 +4,7 @@ const Image = require("@11ty/eleventy-img");
 module.exports = async function(src, alt, sizes, loading = "lazy") {
   let filePath = src;
   if ( src.toString().startsWith('/static') ) {
-    filePath = src.toString().replace('/static', '/src/static');
+    filePath = src.toString().replace('/static', 'src/static');
   }
   let metadata = await Image(filePath, {
     widths: [300, 600, 900, 1200, 1500],
