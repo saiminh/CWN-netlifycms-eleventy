eb589bfa1281a48b4de25fce4065650d5abf9017
saiminh <saiminh@gmail.com>
2023-03-02 16:47:42 +0100

    don't change /static to src/static

--- src/_includes/_components/shortcodes/imageShortcode.js
+++ src/_includes/_components/shortcodes/imageShortcode.js
@@ -3,9 +3,9 @@ const Image = require("@11ty/eleventy-img");
 // Async Image Shortcode function
 module.exports = async function(src, alt, sizes, loading = "lazy") {
   let filePath = src;
-  if ( src.toString().startsWith('/static') ) {
-    filePath = src.toString().replace('/static', 'src/static');
-  }
+  // if ( src.toString().startsWith('/static') ) {
+  //   filePath = src.toString().replace('/static', 'src/static');
+  // }
   let metadata = await Image(filePath, {
     widths: [300, 600, 900, 1200, 1500],
     formats: ["avif", "jpeg"],
@@ -17,8 +17,7 @@ module.exports = async function(src, alt, sizes, loading = "lazy") {
     alt,
     sizes,
     loading: loading,
-    decoding: "async",
-    lemmesee: src
+    decoding: "async"
   };
 
   // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
